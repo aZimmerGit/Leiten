f77e095dde149886448b6f9465dc395a97a7557e
Alexander Zimmerman <anderzimmerman@yahoo.com>
2019-11-15 13:38:45 -0600

    map markers

--- screens/MapView.js
+++ screens/MapView.js
@@ -1,12 +1,54 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import storage from '../store/memStore'
+
+
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      initialRegion: {
+        latitude: 41.8900,
+        longitude: -87.6500,
+        latitudeDelta: 0.1900,
+        longitudeDelta: 0.0421,
+      },
+      active: {
+        latitude: 41.9351802,
+        longitude: -87.6465491,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      storage: storage
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.state.storage[1])
+    console.log(this.state.active)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle} />
+        <MapView
+      initialRegion={this.state.initialRegion}
+      style={styles.mapStyle}>
+
+        {this.state.storage.map(mark => {
+          return (
+          <Marker
+          key={mark.id}
+          title={mark.name}
+          coordinate={mark.latlng}
+          />
+          )
+        })}
+
+      </MapView>
       </View>
     );
   }
---
+++ store/memStore.js
@@ -0,0 +1,58 @@
+const storage = [
+  {
+    id: 1,
+    name: 'The Gundis Kurdish Kitchen',
+    latlng: {
+      latitude: 41.9351802,
+      longitude: -87.6465491,
+    },
+    streetAddress: '2909 N Clark St, Chicago, IL 60657',
+    filtOpts: [
+      'Kurdish',
+      'Comfort Food',
+      'Ambient',
+      'Vegetarian Options',
+      'Vegan Options',
+    ],
+    phone: 7739048120,
+    website: 'https://thegundis.com/',
+  },
+  {
+    id: 2,
+    name: 'Daisies',
+    latlng: { latitude: 41.9272894, longitude: -87.7063449 },
+
+    streetAddress: '2523 N Milwaukee Ave, Chicago, IL 60647',
+    filtOpts: [
+      'Pasta',
+      'Chef-Driven',
+      'Ambient',
+      'Great Deal',
+      'Vegetarian Options',
+      'Vegan Options',
+    ],
+    phone: 7736611671,
+    website: 'https://www.daisieschicago.com/',
+  },
+  {
+    id: 3,
+    name: 'Bad Hunter',
+    latlng: {
+      latitude: 41.8846237,
+      longitude: -87.6498989,
+    },
+
+    streetAddress: '802 W Randolph St, Chicago, IL 60607',
+    filtOpts: [
+      'Vegetable Forward',
+      'New American',
+      'Ambient',
+      'Vegetarian Options',
+      'Vegan Options',
+    ],
+    phone: 3122651745,
+    website: 'https://badhunter.com/',
+  },
+];
+
+export default storage;
